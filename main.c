2eae75799d9ca9ffbf6f4e711eb8d4b3090d3831
BR <bayoeboyy@gmail.com>
2014-01-21 09:42:55 +0700

    ++++++++++++

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printf("++++++++++++++++++++++++++++++++++++++++++++++\n");
+  
+  system("PAUSE");	
+  return 0;
+}
